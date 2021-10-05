1041c9903373b077c81df8f5ee589643ee69a441
Douglas <dougdwfp@gmail.com>
2021-10-05 19:53:26 -0300

    trab01

---
+++ trab1/Trab01/Trab01/Trab01/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Trab01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_Clicked(object sender, EventArgs e)
+        {
+            if (login.Text == "admin" && senha.Text == "senha@admin")
+            {
+                DisplayAlert("Mensagem", "Logado com Sucesso", "OK");
+            }
+            else
+            {
+                DisplayAlert("Mensagem", "Login não Autorizado", "OK");
+            }
+        }
+
+        private void Limpar_Clicked(object sender, EventArgs e)
+        {
+            login.Text = "";
+            senha.Text = "";
+        }
+
+        private void Cred_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Creditos", " Douglas Willian CB3005348 e João Vitor Gino CB3005488 ", "OK");
+        }
+    }
+}
